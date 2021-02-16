4542acca497f6e82f2701f996e8ac6f9cf56acae
DavidMaximaWang <qunlingwang@gmail.com>
2021-02-16 13:52:07 +0000

    small clean up

--- src/features/auth/authReducer.js
+++ src/features/auth/authReducer.js
@@ -7,7 +7,6 @@ const initialState = {
 }
 
 const loginUser = (state, payload)=>{
-    console.log(state, payload)
     return {
         authenticated: true,
         currentUser:payload.creds.email
